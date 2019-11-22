a41be2dc9a87ffbf799097fd5c08d0b5271e1d50
Yuto Watanabe <yuto.w51942@gmail.com>
2019-11-22 14:23:29 +0900

    added tests.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Auto-C-Execution",
+    version="1.0.0",
+    description="""Auto-C-Execution""",
+    url="https://github.com/yuto51942/Auto-C-Execution.git",
+    install_requires=[],
+    packages=['main']
+)
